b0e83a6ca91239572abb5345e037c4bcf926eeb2
Markus Heiliger <markus.heiliger@dash-two.de>
2018-12-05 17:46:16 -0800

    improved runner

--- container/tfrunner.sh
+++ container/tfrunner.sh
@@ -9,7 +9,7 @@ trace "Setup folder structure ..."
 mkdir /runbooks && cd /runbooks
 
 trace "Downloading runbooks ..."
-for url in $*; do wget ${url}; done
+for url in $*; do wget –quiet ${url}; done
 
 trace "Cleanup runbooks ..."
 for file in $(find -type f -name "*\?*"); do mv $file $(echo $file | cut -d? -f1); done
@@ -17,7 +17,7 @@ for file in $(find -type f -name "*\?*"); do mv $file $(echo $file | cut -d? -f1
 trace "Connecting Azure ..."
 while true; do
     # managed identity isn't avaialble directly - retry
-    az login --identity 2>/dev/null && {
+    az login --identity > /dev/null && {
         export ARM_USE_MSI=true
         export ARM_MSI_ENDPOINT='http://169.254.169.254/metadata/identity/oauth2/token'
         export ARM_SUBSCRIPTION_ID=$(az account show --output=json | jq -r -M '.id')
@@ -34,10 +34,7 @@ terraform init
 trace "Applying Terraform ..."
 terraform apply -auto-approve -var "EnvironmentResourceGroupName=$EnvironmentResourceGroupName"
 
-if [ -z "$ContainerGroupId" ]; then
-    trace "Waiting for termination ..."
-    tail -f /dev/null
-else
+if [[!! -z "$ContainerGroupId" ]]; then
     trace "Deleting container groups ..."
     az container delete --yes --ids $ContainerGroupId
 fi
