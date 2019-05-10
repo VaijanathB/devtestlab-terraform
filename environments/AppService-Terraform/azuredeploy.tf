cf378b52a263facc2f40d788b96b4b65ccb46bf6
Vaijanath Angadihiremath <vaangadi@VJ-macpro.guest.corp.microsoft.com>
2019-05-10 15:16:38 -0700

    Typo.

--- environments/AppService-Terraform/azuredeploy.tf
+++ environments/AppService-Terraform/azuredeploy.tf
@@ -17,13 +17,13 @@ data "azurerm_resource_group" "test" {
 resource "azurerm_virtual_network" "test" {
   name                = "acceptanceTestVirtualNetwork1"
   address_space       = ["10.0.0.0/16"]
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
+  location            = "${data.azurerm_resource_group.test.location}"
+  resource_group_name = "${data.azurerm_resource_group.test.name}"
 }
 
 resource "azurerm_subnet" "test" {
   name                 = "testsubnet"
-  resource_group_name  = "${azurerm_resource_group.test.name}"
+  resource_group_name  = "${data.azurerm_resource_group.test.name}"
   virtual_network_name = "${azurerm_virtual_network.test.name}"
   address_prefix       = "10.0.1.0/24"
   delegation {
